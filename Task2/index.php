8d7eb3a38b523e374abfed167be4240ab7fe9bcb
coolbuch <kovdima2000@gmail.com>
2020-04-11 13:59:21 +0800

    all break

---
+++ Task2/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>
+        Таблица умножения
+    </title>
+    <link rel="stylesheet" href="style.css"/>
+    <meta charset="UTF-8">
+</head>
+<body>
+<table cellspacing="0">
+    <?php
+    $n = 10;
+    echo "<tr><td class='first'></td>";
+    for ($i = 1; $i < $n + 1; $i++)
+    {
+        echo "<td>$i</td>";
+    }
+    echo "</tr>";
+    for ($i = 1; $i < $n + 1; $i++)
+    {
+        echo "<tr><td>$i</td>";
+        for ($j = 1; $j < $n + 1; $j++)
+        {
+            $res = $i * $j;
+            if ($i == $j)
+                echo "<td class='diagonal'>$res</td>";
+            else
+            {
+                echo '<td>'.$res.'</td>';
+            }
+
+        }
+        echo "</tr>";
+    }
+    ?>
+</table>
+</body>
+
+</html>
